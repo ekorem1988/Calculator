2bb71ee170c6d0ad79fd884f90e1db140c8f2100
ekorem1988 <83618712+ekorem1988@users.noreply.github.com>
2022-04-18 12:01:15 +0200

    bug fix u app modle nije dobar import bio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyCalcComponent } from './my-calc/my-calc.component';
@@ -15,7 +15,7 @@ import { MyCalcComponent } from './my-calc/my-calc.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    NgModel
+  
   ],
   
   providers: [],
